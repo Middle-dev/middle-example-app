fbe4a7368e8c44f40edd8e7d32b1a6a31ce10187
Adam Easterling <adam@middle.app>
2023-09-11 21:15:53 -0700

    more work on workflows

--- README.md
+++ README.md
@@ -4,10 +4,10 @@ An example for how to create an app in Middle
 ## Building
 Once you're ready to test your app on Middle, compile it with the following command:
 
-    cargo wasi build
+    cargo build --target wasm32-unknown-unknown
 
 The above will compile a Web Assembly binary that's rather large, as it has all the debug symbols turned on. This is great for debugging.
 
 However, if you want a smaller Web Assembly binary, you can build with the following command.
 
-    cargo wasi build --release
+    cargo build --target wasm32-unknown-unknown --release
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,39 @@
+use std::time::Duration;
+
 use middle_wasm::prelude::*;
 
-#[middle_fn("This function returns `Hello, {input.name}`")]
+
+#[middle_fn]
+/// This function returns `Hello, {input.name}`
 fn make_hello(name: String) -> String {
     format!("Hello, {name}!")
+}
+
+#[middle_workflow]
+/// This is a test workflow, which can pause, and stop and resume later.
+fn test_workflow(to_print: String) -> Resumable<String> {
+    // This function prints something to Middle's console.
+    mprint("making a request...");
+
+    // This code will repeatedly execute until a non-500 error is returned.
+    // There's an upper-bound to the number of steps executed, after which an error will result.
+    let out = loop {
+        let out = middle_wasm::RequestBuilder::get("https://middle.app")
+            .call()
+            .unwrap();
+
+        if out.code() >= 500 {
+            // Waiting 5 seconds for 5xx error to resolve
+            middle_wasm::pause(Duration::from_secs(5))?;
+        } else {
+            break out;
+        }
+    };
+    
+    // Code from this point on will only be run once the pause duration has passed.
+    mprint(format!("we were supposed to print... {to_print}"));
+    mprint(format!("here's the result of our call... {}", out.code()));
+    
+    // We have to return Ready when the workflow is done.
+    Resumable::Ready("I'm done!".to_string())
 }
